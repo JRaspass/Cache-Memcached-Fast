4676cd43575b3e3ae7a71a240852e7f64f1f5396
Tomash Brechko <tomash.brechko@gmail.com>
2007-11-26 12:16:26 +0300

    Remove Exporter stuff, we do not export anything.

--- lib/Cache/Memcached/Fast.pm
+++ lib/Cache/Memcached/Fast.pm
@@ -4,28 +4,9 @@ use 5.006;
 use strict;
 use warnings;
 
-require Exporter;
-
-our @ISA = qw(Exporter);
-
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-# This allows declaration	use Cache::Memcached::Fast ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = ( 'all' => [ qw(
-	
-) ] );
-
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-
-our @EXPORT = qw(
-	
-);
 
 our $VERSION = '0.02';
+
 
 require XSLoader;
 XSLoader::load('Cache::Memcached::Fast', $VERSION);
