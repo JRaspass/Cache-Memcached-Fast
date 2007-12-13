b30be3322fd3fabf1fa2b0355fca0442b5ccc37b
Tomash Brechko <tomash.brechko@gmail.com>
2007-12-13 17:35:25 +0300

    Add module documentation.

--- lib/Cache/Memcached/Fast.pm
+++ lib/Cache/Memcached/Fast.pm
@@ -1,3 +1,84 @@
+=head1 NAME
+
+Cache::Memcached::Fast - Perl client for
+L<memcached|http://www.danga.com/memcached/>, in C language
+
+=head1 SYNOPSIS
+
+  use Cache::Memcached::Fast;
+
+  my $memd = new Cache::Memcached::Fast({
+      servers => [('localhost:11211') x 2, '192.168.254.2:11211',
+                  '/path/to/unix.sock'],
+      namespace => 'my:',
+      connect_timeout => 0.2,
+      io_timeout => 0.5,
+      close_on_error => 1,
+      compress_threshold => 100_000,
+      compress_algo => 'deflate',
+      max_failures => 3,
+      failure_timeout => 2,
+  });
+
+  # Store scalars.
+  $memd->add('skey', 'text');
+  $memd->replace('skey', 'val');
+  $memd->set('nkey', 5);
+
+  # Store arbitrary Perl data structures.
+  my %hash = (a => 1, b => 2);
+  $memd->set('hash', \%hash);
+
+  # Add to strings.
+  $memd->prepend('skey', 'This is a ');
+  $memd->append('skey', 'ule.');
+
+  # Do arithmetic.
+  $memd->incr('nkey', 10);
+  print "OK\n" if $memd->decr('nkey', 3) == 12;
+
+  # Retrive values.
+  my $val = $memd->get('skey');
+  print "OK\n" if $val eq 'This is a value.';
+  my $href = $memd->get_multi('hash', 'nkey');
+  print "OK\n" if $$href{hash}->{b} == 2 and $$href{nkey} == 12;
+
+  # Do atomic test-and-set operations.
+  my $cas_val = $memd->gets('nkey');
+  $$cas_val[1] = 0 if $$cas_val[1] == 12;
+  if ($memd->cas('nkey', @$cas_val)) {
+      print "OK, value updated\n";
+  } else {
+      print "Update failed, probably another client"
+          . " has updated the value\n";
+  }
+
+  # Delete some data.
+  $memd->delete('skey');
+
+  # Wipe out all cached data.
+  $memd->flush_all;
+
+
+=head1 DESCRIPTION
+
+B<Cache::Memcahced::Fast> is a Perl client for memcached, a memory
+cache daemon.  Client core is implemented in C and tries hard to
+minimize number of system calls and to avoid any key/value copying for
+speed.
+
+API is largely compatible with L<Cache::Memcached|Cache::Memcached>,
+most users may start using this module by installing it and adding
+I<"::Fast"> to the old name in their scripts (see L<"Compatibility
+with Cache::Memcached"> below for full details).
+
+
+=head2 Export
+
+None.  Module has object oriented API.
+
+=cut
+
 package Cache::Memcached::Fast;
 
 use 5.006;
@@ -56,6 +137,177 @@ use fields qw(
 );
 
 
+=head2 Constructor
+
+  my $memd = new Cache::Memcached::Fast($params);
+
+Create new client object.  I<$params> is a reference to a hash with
+client parameters.  Currently recognized keys are:
+
+=over
+
+=item I<servers>
+
+  servers => [('localhost:11211') x 2, '192.168.254.2:50000',
+              '/path/to/unix.sock']
+  (defalut: none)
+
+The value is a reference to an array of server addresses.  Each
+address is either I<host:port> for network TCP connections, or
+F</path/to/unix.sock> for local Unix socket connections.
+
+
+=item I<namespace>
+
+  namspace => 'my::'
+  (default: '')
+
+The value is a scalar that will be prepended to all key names passed
+to the memcached server.  By using different namespaces clients avoid
+interference with each other.
+
+
+=item I<connect_timeout>
+
+  connect_timeout => 0.7
+  (default: 0.25 seconds)
+
+The value is a rational number of seconds to wait for connect to
+complete.  Applies only to network connections.
+
+Note that network connect process consists of several steps:
+destination host address lookup, which may return several addresses in
+general case (expecially for IPv6, see
+L<http://people.redhat.com/drepper/linux-rfc3484.html> and
+L<http://people.redhat.com/drepper/userapi-ipv6.html>), then the
+attempt to connect to one of those addresses.  I<connect_timeout>
+applies only to one such connect, i.e. to one L<connect(2)|connect>
+call.  Thus overall connect process may take longer than
+I<connect_timeout> seconds, but this is unavoidable.
+
+
+=item I<io_timeout> (or deprecated I<select_timeout>)
+
+  io_timeout => 0.5
+  (default: 1.0 seconds)
+
+The value is a rational number of seconds to wait before giving up on
+communicating with the server(s).
+
+Note that for commands that communicate with more than one server
+(like L<get_multi>) the timeout applies per server set, not per each
+server.  Thus it won't expire if one server is quick enough to
+communicate, eevn if others are silent.  But if some servers are dead
+those alive will finish communication, and then dead servers would
+timeout.
+
+
+=item I<close_on_error>
+
+  close_on_error => 0
+  (defalut: enabled)
+
+The value is a boolean which enables (true) or disables (false)
+I<close_on_error> mode.  When enabled, any error response from the
+memcached server would make client to close the connection.  Note that
+such "error response" is different from "negative response".  The
+latter means the server processed the command and yield negative
+result.  The former means the server failed to process the command for
+some reason.  I<close_on_error> is enabled by default for safety.
+Consider the following:
+
+=over
+
+=item 1 client want to set some value, but mistakenly sends malformed
+        command (this can't happen with current module of course ;)):
+
+  set key 10\r\n
+  value_data\r\n
+
+=item 2 memcahced server reads first line, 'set key 10', and can't
+        parse it, because there's wrong number of tokens in it.  So it
+        sends
+
+  ERROR\r\n
+
+=item 3 then the server reads 'value_data' while it is in
+        accept-command state!  It can't parse it either (hopefully),
+        and sends again
+
+  ERROR\r\n
+
+=back
+
+But the client expects one reply per command, so after sending the
+next command it willthink that the second 'ERROR' is a reply for this
+new command.  This menas that all replies would shift, including
+replies for L<get> commands!  By closing the connection we avoid such
+possibility.
+
+When connection dies, or the client receives the reply that it can't
+understand, it closes its end regardless the I<close_on_error>
+setting.
+
+
+=item I<compress_threshold>
+
+  compress_threshold => 10_000
+  (default: -1)
+
+The value is an integer.  When positive it denotes the threshold size
+in bytes: data with the size equal or larger than this should be
+compressed.  See L<compress_ratio> and L<compress_algo> below.
+
+Negative value disables compression.
+
+
+=item I<compress_ratio>
+
+  compress_ratio => 0.6
+  (default: 0.8)
+
+Tha value is a fractional number between 0 and 1.  When
+L<compress_threshold> triggers the compression, compressed size should
+be less or equal to S<original-size * I<compress_ratio>>.  Otherwise
+the data will be stored uncompressed.
+
+
+=item I<compress_algo>
+
+  compress_algo => 'bzip2'
+  (default: 'gzip')
+
+The value is a scalar with the name of the compression algorithm
+(currently known are 'gzip', 'zip', 'bzip2', 'deflate', 'rawdeflate',
+'lzop', 'lzf').  You have to have corresponding IO::Compress::<Algo>
+installed, otherwise the module will give a warning and compression
+will be disabled.
+
+
+=item I<max_failures>
+
+  max_failures => 3
+  (default: 0)
+
+The value is a non-negative integer.  When positive, if there happend
+I<max_falures> in I<failure_timeout> seconds, the client does not try
+to connect to this particular server for another I<failure_timeout>
+seconds.  Value of zero disables this behaviour.
+
+
+=item I<failure_timeout>
+
+  failure_timeout => 30
+  (default: 10 seconds)
+
+The value is a positive integer number of seconds.  See
+L<max_failures>.
+
+
+=back
+
+=cut
+
 sub new {
     my $class = shift;
     my ($conf) = @_;
@@ -84,6 +336,24 @@ sub DESTROY {
     # of Cache::Memcached::Fast::_xs via AUTOLOAD.
 }
 
+
+=head2 Methods
+
+=over
+
+=item C<enable_compress>
+
+  $memd->enable_compress($enable);
+
+Enable compression when boolean I<$enable> is true, disable when false.
+
+Note that you can enable compression only when you set
+L<compress_threshold> to some positive value and L<compress_algo>
+holds the name of a known compression algorithm.
+
+I<Return:> none.
+
+=cut
 
 sub enable_compress {
     my Cache::Memcached::Fast $self = shift;
@@ -160,12 +430,45 @@ sub _unpack_value {
 }
 
 
+=item C<set>
+
+  $memd->set($key, $value);
+  $memd->set($key, $value, $expiration_time);
+
+Store the I<$value> on the server under the I<$key>.  I<$key> should be a
+scalar.  I<$value> should be defined and may be of any Perl data type.
+When it is a reference, the referenced Perl data structure will be
+transparently serialized with L<Storable|Storable> module.
+
+Optional I<$expiration_time> is a positive integer number of seconds
+after which the value will expire and wouldn't be accessible any
+longer.
+
+I<Return:> boolean, true if operation succeeded, false otherwise.
+
+=cut
+
 sub set {
     my Cache::Memcached::Fast $self = shift;
     splice(@_, 1, 1, _pack_value($self, $_[1]));
     return $self->{_xs}->set(@_);
 }
 
+
+=item C<cas>
+
+  $memd->cas($key, $cas, $value);
+  $memd->cas($key, $cas, $value, $exiration_time);
+
+Store the I<$value> on the server under the I<$key>, but only if CAS value
+associated with this key is equal to I<$cas>.  I<$cas> is an opaque object
+returned with L<gets> or L<gets_multi>.
+
+See L<set> for I<$key>, I<$value>, I<$expiration_time> parameters description.
+
+I<Return:> boolean, true if operation succeeded, false otherwise.
+
+=cut
 
 sub cas {
     my Cache::Memcached::Fast $self = shift;
@@ -174,6 +477,20 @@ sub cas {
 }
 
 
+=item C<add>
+
+  $memd->add($key, $value);
+  $memd->add($key, $value, $exiration_time);
+
+Store the I<$value> on the server under the I<$key>, but only if the key
+B<doesn't> exists on the server.
+
+See L<set> for I<$key>, I<$value>, I<$expiration_time> parameters description.
+
+I<Return:> boolean, true if operation succeeded, false otherwise.
+
+=cut
+
 sub add {
     my Cache::Memcached::Fast $self = shift;
     splice(@_, 1, 1, _pack_value($self, $_[1]));
@@ -181,12 +498,40 @@ sub add {
 }
 
 
+=item C<replace>
+
+ $memd->replace($key, $value);
+ $memd->replace($key, $value, $exiration_time);
+
+Store the I<$value> on the server under the I<$key>, but only if the key
+B<does> exists on the server.
+
+See L<set> for I<$key>, I<$value>, I<$expiration_time> parameters description.
+
+I<Return:> boolean, true if operation succeeded, false otherwise.
+
+=cut
+
 sub replace {
     my Cache::Memcached::Fast $self = shift;
     splice(@_, 1, 1, _pack_value($self, $_[1]));
     return $self->{_xs}->replace(@_);
 }
 
+
+=item C<append>
+
+  $memd->append($key, $value);
+
+B<Append> the I<$value> to the current value on the server under the
+I<$key>.
+
+I<$key> and I<$value> should be scalars, as well as current value on the
+server.  C<append> doesn't affect expiration time of the value.
+
+I<Return:> boolean, true if operation succeeded, false otherwise.
+
+=cut
 
 sub append {
     my Cache::Memcached::Fast $self = shift;
@@ -196,6 +541,20 @@ sub append {
 }
 
 
+=item C<prepend>
+
+  $memd->prepend($key, $value);
+
+B<Prepend> the I<$value> to the current value on the server under the
+I<$key>.
+
+I<$key> and I<$value> should be scalars, as well as current value on the
+server.  C<append> doesn't affect expiration time of the value.
+
+I<Return:> boolean, true if operation succeeded, false otherwise.
+
+=cut
+
 sub prepend {
     my Cache::Memcached::Fast $self = shift;
     # prepend() does not affect flags.
@@ -203,6 +562,16 @@ sub prepend {
     return $self->{_xs}->prepend(@_);
 }
 
+
+=item C<get>
+
+  $memd->get($key);
+
+Retrive the value for a I<$key>.  I<$key> should be a scalar.
+
+I<Return:> value accociated with the I<$key>, or I<undef>.
+
+=cut
 
 sub get {
     my Cache::Memcached::Fast $self = shift;
@@ -216,6 +585,17 @@ sub get {
     }
 }
 
+
+=item C<get_multi>
+
+  $memd->get_multi(@keys);
+
+Retrive several values associated with I<@keys>.  I<@keys> should be an
+array of scalars.
+
+I<Return:> reference to hash, where I<$$href{$key}> holds corresponding value.
+
+=cut
 
 sub get_multi {
     my Cache::Memcached::Fast $self = shift;
@@ -236,6 +616,22 @@ sub get_multi {
 }
 
 
+=item C<gets>
+
+  $memd->gets($key);
+
+Retrive the value and its CAS for a I<$key>.  I<$key> should be a scalar.
+
+I<Return:> reference to an array I<[$cas, $value]>.  You may conveniently pass
+it back to L<cas> with
+
+  my $cas_val = $memd->gets($key);
+  # Update value.
+  $$cas_val[1] = 3;
+  $memd->cas($key, @$cas_val);
+
+=cut
+
 sub gets {
     my Cache::Memcached::Fast $self = shift;
 
@@ -249,6 +645,18 @@ sub gets {
     }
 }
 
+
+=item C<gets_multi>
+
+  $memd->gets_multi(@keys);
+
+Retrive several values and their CASs associated with I<@keys>.  I<@keys>
+should be an array of scalars.
+
+I<Return:> reference to hash, where I<$$href{$key}> holds a reference to an
+array I<[$cas, $value]>.  Compare with L<gets>.
+
+=cut
 
 sub gets_multi {
     my Cache::Memcached::Fast $self = shift;
@@ -269,6 +677,35 @@ sub gets_multi {
 }
 
 
+=item C<delete> (or deprecated C<remove>)
+
+  $memd->delete($key);
+  $memd->delete($key, $delay);
+
+Delete I<$key> and its value from the cache.  I<$delay> is an optional
+non-negative integer number of seconds to delay the operation.  During
+this time L<add> and L<replace> commands will be rejected by the
+server.  When ommited, zero is assumed, i.e. delete immediatly.
+
+I<Return:> boolean, true if operation succeeded, false otherwise.
+
+
+=item C<flush_all>
+
+  $memd->flush_all;
+  $memd->flush_all($delay);
+
+Flush all caches the client knowns about.  I<$delay> is an optional
+non-negative integer number of seconds to delay the operation.  The
+delay will be distributed across the servers.  For instance, when you
+have three servers, and call C<flush_all(30)>, the servers would get
+30, 15, 0 seconds delays respectively.  When ommited, zero is assumed,
+i.e. flush immediatly.
+
+I<Return:> boolean, true if operation succeeded, false otherwise.
+
+=cut
+
 sub AUTOLOAD {
     my Cache::Memcached::Fast $self = shift;
     my ($method) = $AUTOLOAD =~ /::([^:]+)$/;
@@ -278,53 +715,151 @@ sub AUTOLOAD {
 
 1;
 __END__
-# Below is stub documentation for your module. You'd better edit it!
 
-=head1 NAME
+=back
 
-Cache::Memcached::Fast - Perl extension for blah blah blah
 
-=head1 SYNOPSIS
+=head2 Compatibility with Cache::Memcached
 
-  use Cache::Memcached::Fast;
-  blah blah blah
+This module is designed to be a drop in replacement for
+L<Cache::Memcached|Cache::Memcached>.  Internally
+Cache::Memcached::Fast uses the same hash function as
+Cache::Memcached, and thus should distribute the keys across several
+servers the same way.  So both modules may be used interchangeably.
+Most users should be able to use this module after replacing
+I<"Cache::Memcached"> with I<"Cache::Memcached::Fast">, without
+further code modifications.  However, as of this release, the
+following features of Cache::Memcached are not supported by
+Cache::Memcached::Fast (and some of them will never be):
 
-=head1 DESCRIPTION
+=head3 Constructor parameters
 
-Stub documentation for Cache::Memcached::Fast, created by h2xs. It
-looks like the author of the extension was negligent enough to leave
-the stub unedited.
+=over
 
-Blah blah blah.
+=item I<servers>
 
-=head2 EXPORT
+The syntax
 
-None by default.
+  servers => [['host:port', $weight]]
 
+is not supported.  You may workaround this by adding the server
+several times with the syntax
+
+  servers => [('host:port') x $weight]
+
+or just wait for server weight support, which is planned.
+
+
+=item I<no_rehash>
+
+Current implementation never rehashes keys, instead L<max_failures>
+and L<failure_timeout> are used.
+
+
+=item I<readonly>
+
+Not supported.  Easy to add.  However I'm not sure about the demand
+fot it, and it will slow down things a bit (because from design point
+of view it's better to add it on Perl side rather than on XS side).
+
+
+=item I<debug>
+
+Not supported.  Since the implementation is different, there can't be
+any compatibility on I<debug> level.
+
+=back
+
+
+=head3 Methods
+
+=over
+
+=item Note on keys
+
+Every key should be a scalar.  The syntax when key is a reference to
+an array I<[$key, $precomputed_hash]> is not supported.
+
+
+=item C<set_servers>
+
+Not supported.  It is planned to add consistent hashing and server
+add/remove.  In the meanwhile server set should not change after
+client object construction.
+
+
+=item C<set_debug>
+
+Not supported.  See L<debug>.
+
+
+=item C<set_readonly>
+
+Not supported.  See L<readonly>.
+
+
+=item C<set_norehash>
+
+Not supported.  See L<no_rehash>.
+
+
+=item C<set_compress_threshold>
+
+Not supported.  Easy to add.  Currently you specify
+I<compress_threshold> during client object construction.
+
+
+=item C<stats>
+
+Not supported.  Perhaps will appear in the future releases.
+
+
+=item C<disconnect_all>
+
+Not supported.  Easy to add.  Meanwhile to disconnect from all servers
+you may do
+
+  undef $memd;
+
+or 
+
+  $memd = undef;
+
+=back
 
 
 =head1 SEE ALSO
 
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
+L<Cache::Memcached|Cache::Memcached> - original pure Perl memcached
+client.
 
-If you have a mailing list set up for your module, mention it here.
+L<http://www.danga.com/memcached/> - memcached website.
 
-If you have a web site set up for your module, mention it here.
 
-=head1 AUTHOR
+=head1 AUTHORS
 
-Tomash Brechko, E<lt>tomash.brechko@gmail.comE<gt>
+Tomash Brechko, E<lt>tomash.brechko@gmail.comE<gt> - design and
+implementation.  Feel free to send bug reports and patches to this
+address.
+
+Michael Monashev, E<lt>postmaster@softsearch.ruE<gt> - project
+management, design suggestions, testing.
+
+Development sponsored by Michael Monashev.
+
+
+=head1 WARRANTY
+
+There's B<NONE>, neither explicit nor implied.  But you knew it already
+;).
+
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2007 by Tomash Brechko
+Copyright (C) 2007 by Tomash Brechko.  All rights reserved.
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.8.8 or,
 at your option, any later version of Perl 5 you may have available.
-
 
 =cut
