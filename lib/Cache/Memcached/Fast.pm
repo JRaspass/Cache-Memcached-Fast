1b90bfef36b8d2914d285d443c2a7da07fcc5fa4
Tomash Brechko <tomash.brechko@gmail.com>
2007-12-15 20:26:40 +0300

    Increase version number to 0.03.

--- lib/Cache/Memcached/Fast.pm
+++ lib/Cache/Memcached/Fast.pm
@@ -15,11 +15,11 @@ L<memcached|http://www.danga.com/memcached/>, in C language
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 
 =head1 SYNOPSIS
