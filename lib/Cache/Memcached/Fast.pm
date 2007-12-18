b0cf8f69a6697ae690d6e7cd6ae30ae52c2ea1d5
Tomash Brechko <tomash.brechko@gmail.com>
2007-12-18 15:22:29 +0300

    Increase version number to 0.06.

--- lib/Cache/Memcached/Fast.pm
+++ lib/Cache/Memcached/Fast.pm
@@ -14,11 +14,11 @@ Cache::Memcached::Fast - Perl client for B<memcached>, in C language
 
 =head1 VERSION
 
-Version 0.05
+Version 0.06
 
 =cut
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 
 =head1 SYNOPSIS
