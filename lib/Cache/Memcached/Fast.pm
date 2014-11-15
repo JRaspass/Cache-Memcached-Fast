e2234868bf6c220a86128941da8cf587a10b8ea3
Tomash Brechko <tomash.brechko@gmail.com>
2014-11-15 19:42:10 +0300

    Increment version to 0.23 and update Changes file.

--- lib/Cache/Memcached/Fast.pm
+++ lib/Cache/Memcached/Fast.pm
@@ -14,11 +14,11 @@ Cache::Memcached::Fast - Perl client for B<memcached>, in C language
 
 =head1 VERSION
 
-Version 0.22.
+Version 0.23.
 
 =cut
 
-our $VERSION = '0.22';
+our $VERSION = '0.23';
 
 
 =head1 SYNOPSIS
