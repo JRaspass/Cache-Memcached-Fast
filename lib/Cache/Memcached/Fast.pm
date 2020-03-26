757aaf82dc69375e2ecefb11aba6385f3bd133f2
James Raspass <jraspass@gmail.com>
2020-03-26 15:45:36 +0000

    Release 0.26

--- lib/Cache/Memcached/Fast.pm
+++ lib/Cache/Memcached/Fast.pm
@@ -14,11 +14,11 @@ Cache::Memcached::Fast - Perl client for B<memcached>, in C language
 
 =head1 VERSION
 
-Version 0.25.
+Version 0.26.
 
 =cut
 
-our $VERSION = '0.25';
+our $VERSION = '0.26';
 
 
 =head1 SYNOPSIS
