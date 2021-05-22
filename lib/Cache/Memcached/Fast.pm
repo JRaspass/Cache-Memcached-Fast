e69eccd5f3d7015db73ecaf513e0d1a9f33746f6
James Raspass <jraspass@gmail.com>
2021-05-22 19:19:58 +0100

    Bump version

--- lib/Cache/Memcached/Fast.pm
+++ lib/Cache/Memcached/Fast.pm
@@ -14,11 +14,11 @@ Cache::Memcached::Fast - Perl client for B<memcached>, in C language
 
 =head1 VERSION
 
-Version 0.26.
+Version 0.27.
 
 =cut
 
-our $VERSION = '0.26';
+our $VERSION = '0.27';
 
 
 =head1 SYNOPSIS
